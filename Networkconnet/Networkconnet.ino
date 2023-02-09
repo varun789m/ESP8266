f58857f36196fd44fb3853d694c2a38792a285dc
varun789m <94894837+varun789m@users.noreply.github.com>
2023-02-09 23:52:27 +0530

    Update Networkconnet.ino

--- Networkconnet/Networkconnet.ino
+++ Networkconnet/Networkconnet.ino
@@ -1,19 +1,23 @@
+// <ESP8266WiFi.h> It is for the module liberary.
 #include <ESP8266WiFi.h>
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println();
-
+// Here remove the nameofwifi ,password of wifi and add your wifi password.
   WiFi.begin("nameofwifi", "password of wifi");
 
   Serial.print("Connecting");
+  
   while (WiFi.status() != WL_CONNECTED)
   {
+  // Here you can delay time a which rate it should connet to wifi.
     delay(500);
     Serial.print(".");
   }
   Serial.println();
+// It will show you the ip provided by your wifi.
 
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
