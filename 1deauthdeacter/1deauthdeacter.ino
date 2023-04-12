0b61bfe20f848cf290ebd7a91eba832aa69a0d7f
varun789m <94894837+varun789m@users.noreply.github.com>
2023-04-12 06:32:55 +0530

    Add files via upload

---
+++ 1deauthdeacter/1deauthdeacter.ino
@@ -0,0 +1,128 @@
+// This software is licensed under the MIT License.
+// See the license file for details.
+// For more details visit github.com/spacehuhn/DeauthDetector
+
+// include necessary libraries
+#include <ESP8266WiFi.h>       // For the WiFi Sniffer
+void promiscuousCallback(uint8_t* buf, uint16_t len);
+
+// include ESP8266 Non-OS SDK functions
+extern "C" {
+#include "user_interface.h"
+}
+
+// ===== SETTINGS ===== //
+#define LED_R D8           /* Red LED pin */
+#define LED_G D6           /* Green LED pin */
+#define LED_B D7            /* Blue LED pin */
+#define SERIAL_BAUD 115200 /* Baudrate for serial communication */
+#define CH_TIME 140        /* Scan time (in ms) per channel */
+#define PKT_RATE 5         /* Min. packets before it gets recognized as an attack */
+#define PKT_TIME 1         /* Min. interval (CH_TIME*CH_RANGE) before it gets recognized as an attack */
+
+// Channels to scan on (US=1-11, EU=1-13, JAP=1-14)
+const short channels[] = { 1,2,3,4,5,6,7,8,9,10,11,12,13/*,14*/ };
+
+// ===== Runtime variables ===== //
+int ch_index { 0 };               // Current index of channel array
+int packet_rate { 0 };            // Deauth packet counter (resets with each update)
+int attack_counter { 0 };         // Attack counter
+unsigned long update_time { 0 };  // Last update time
+unsigned long ch_time { 0 };      // Last channel hop time
+
+// ===== Sniffer function ===== //
+void sniffer(uint8_t *buf, uint16_t len) {
+  if (!buf || len < 28) return; // Drop packets without MAC header
+
+  byte pkt_type = buf[12]; // second half of frame control field
+  //byte* addr_a = &buf[16]; // first MAC address
+  //byte* addr_b = &buf[22]; // second MAC address
+
+  // If captured packet is a deauthentication or dissassociaten frame
+  if (pkt_type == 0xA0 || pkt_type == 0xC0) {
+    ++packet_rate;
+  }
+}
+
+// ===== Attack detection functions ===== //
+void attack_started() {
+  analogWrite(LED_R, 120);
+  analogWrite(LED_G, 0);
+  analogWrite(LED_B, 0);
+  Serial.println("ATTACK DETECTED");
+}
+
+void attack_stopped() {
+  analogWrite(LED_R, 0);
+  analogWrite(LED_G, 100);
+  analogWrite(LED_B, 0);
+  Serial.println("ATTACK STOPPED");
+}
+
+// ===== Setup ===== //
+void setup() {
+  Serial.begin(115200); // Start serial communication
+
+  // Init LEDs
+  analogWriteRange(255);
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT);
+
+  analogWrite(LED_R, 0);
+  analogWrite(LED_G, 100);
+  analogWrite(LED_B, 0);
+
+  WiFi.disconnect();                   // Disconnect from any saved or active WiFi connections
+  wifi_set_opmode(STATION_MODE);       // Set device to client/station mode
+  wifi_set_promiscuous_rx_cb(sniffer); // Set sniffer function
+  wifi_set_channel(channels[0]);        // Set channel
+  wifi_promiscuous_enable(true);       // Enable sniffer
+
+  Serial.println("Started \\o/");
+
+}
+
+// ===== Loop ===== //
+void loop() {
+  unsigned long current_time = millis(); // Get current time (in ms)
+
+  // Update each second (or scan-time-per-channel * channel-range)
+  if (current_time - update_time >= (sizeof(channels)*CH_TIME)) {
+    update_time = current_time; // Update time variable
+
+    // When detected deauth packets exceed the minimum allowed number
+    if (packet_rate >= PKT_RATE) {
+      ++attack_counter; // Increment attack counter
+    } else {
+      if(attack_counter >= PKT_TIME) attack_stopped();
+      attack_counter = 0; // Reset attack counter
+    }
+
+    // When attack exceeds minimum allowed time
+    if (attack_counter == PKT_TIME) {
+      attack_started();
+    }
+
+    Serial.print("Packets/s: ");
+    Serial.println(packet_rate);
+
+    packet_rate = 0; // Reset packet rate
+  }
+
+  // Channel hopping
+  if (sizeof(channels) > 1 && current_time - ch_time >= CH_TIME) {
+    ch_time = current_time; // Update time variable
+
+    // Get next channel
+    ch_index = (ch_index+1) % (sizeof(channels)/sizeof(channels[0]));
+    short ch = channels[ch_index];
+
+    // Set channel
+    //Serial.print("Set channel to ");
+    //Serial.println(ch);
+    wifi_set_channel(ch);
+  }
+
+
+}
